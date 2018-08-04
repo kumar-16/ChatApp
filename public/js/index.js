1cd6324098496ffc18031fab7b4033d06abb8bc6
Aman Kumar <amankumar2186@gmail.com>
2018-08-04 18:33:10 +0530

    Brodcasting Message event

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function(){
     console.log('connected to the server');
 
-    socket.emit('createMessage' , {
-        from: 'Aman',
-        text: 'Hello, I successfully sent this message'
-    });
 });
 
 socket.on('disconnect', function() {
